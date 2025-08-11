9200e573cbac97b916ea0d0a6688a98c447e6c99
Scott Opell <me@scottopell.com>
2025-08-11 13:34:15 -0400

    Fix trim trailing whitespace and add lazy lock

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -69,7 +69,7 @@ augroup END
 ]])
 
 -- Trim whitespace on save
-local function trim_whitespace()
+function trim_whitespace()
   local save = vim.fn.winsaveview()
   vim.cmd("keeppatterns %s/\\s\\+$//e")
   vim.fn.winrestview(save)
